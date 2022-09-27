172283ba01cb2dbcfe97571a17a30e412a5f8099
mawialal <60330595+mawialal@users.noreply.github.com>
2022-09-27 15:01:57 -0400

    fix

--- attack.py
+++ attack.py
@@ -2,7 +2,7 @@ import numpy as np
 from torch.autograd import Variable
 import torch as torch
 import copy
-from torch.autograd.gradcheck import zero_gradients
+#from torch.autograd.gradcheck import zero_gradients
 
 import torch.nn as nn
 import torch.nn.functional as F
@@ -78,7 +78,7 @@ def deepfool(image, net, num_classes=10, overshoot=0.02, max_iter=50):
         grad_orig = x.grad.data.cpu().numpy().copy()
 
         for k in range(1, num_classes):
-            zero_gradients(x)
+            x.grand.zero_()
 
             fs[0, I[k]].backward(retain_graph=True)
             cur_grad = x.grad.data.cpu().numpy().copy()
